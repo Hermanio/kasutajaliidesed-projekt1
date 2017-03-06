bb12c581d908718d91605d65b3ddca7e6a5ba0c9
BuildTools <unconfigured@null.spigotmc.org>
2017-03-06 21:32:16 +0200

    parsley testing

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 /**
  * Created by toks on 3/5/17.
  */
+var firstName;
+$(document).ready(
+    function () {
+        firstName = $('#firstName').parsley();
+        $('#firstName').attr('data-parsley-maxlength', 4);
+        checkIfIsValid();
+        firstName.on('field:success', function() {
+            // In here, `this` is the parlsey instance of #some-input
+            console.log('first name field is valid')
+        });
+        firstName.on('field:error', function() {
+            // In here, `this` is the parlsey instance of #some-input
+            console.log('first name field is NOT VALID')
+        });
+    }
+)
+;
+
+function checkIfIsValid() {
+    console.log(firstName.isValid());
+}
